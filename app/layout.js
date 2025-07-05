6922ba276fdc40d8993724576b46f1ac80dc1aec
Naitik Gupta <guptanaitik46@gmail.com>
2025-07-05 18:05:21 +0530

    first commit

---
+++ app/layout.js
@@ -0,0 +1,52 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
+import Header from "@/components/header";
+import { ThemeProvider } from "@/components/theme-provider";
+import { dark } from "@clerk/themes";
+import { FaHeart } from "react-icons/fa"
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "CrackAI",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <head>
+          <link rel="icon" href="/logo.png" sizes="any" />
+        </head>
+        <body className={`${inter.className}`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            <Toaster richColors />
+
+            <footer className="bg-muted/50 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p className="text-center">
+                  Made with <FaHeart className="inline text-red-600 text-xl" /> by Naitik Gupta
+                </p>
+
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ data/testimonial.js
@@ -0,0 +1,24 @@
+export const testimonial = [
+  {
+    quote:
+      "The AI-powered interview prep was a game-changer. Landed my dream job at a top tech company!",
+    author: "Naitik Gupta",
+    image: "/man1.jpg",
+    role: "Software Engineer",
+    className: "w-30 h-30 rounded-full", 
+  },
+  {
+    quote:
+      "The industry insights helped me pivot my career successfully. The salary data was spot-on!",
+    author: "Priyanshi Luthra",
+    image: "/parig.png",
+    role: "Product Manager",
+  },
+  {
+    quote:
+      "My resume's ATS score improved significantly. Got more interviews in two weeks than in six months!",
+    author: "Aniruddh Pathak",
+    image: "/fit.jpg",
+    role: "Marketing Director",
+  },
+];
